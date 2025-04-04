92be3861eef606a92286a266c19022144dd492f5
Denis <anjunabeats@MacBook-Air-Denis.local>
2025-04-04 15:13:17 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,80 @@
+const popupMenu = document.querySelector('#popup-menu');
+const menuList = document.querySelector("#nav-list").cloneNode(1);
+const burgerBtn = document.querySelector('.header__burger');
+// const body = document.body;
+
+function renderPopup() {
+    popupMenu.appendChild(menuList);
+};
+
+function hambHandler(e) {
+    e.preventDefault();
+    popupMenu.classList.toggle('open');
+    burgerBtn.classList.toggle('active');
+    // body.classList.toggle('lock');
+    renderPopup();
+};
+
+burgerBtn.addEventListener("click", hambHandler);
+
+
+// Полифилл для метода forEach для NodeList
+if (window.NodeList && !NodeList.prototype.forEach) {
+	NodeList.prototype.forEach = function (callback, thisArg) {
+		thisArg = thisArg || window;
+		for (var i = 0; i < this.length; i++) {
+			callback.call(thisArg, this[i], i, this);
+		}
+	};
+}
+
+document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
+	const dropDownBtn = dropDownWrapper.querySelector('.dropdown__button');
+	const dropDownList = dropDownWrapper.querySelector('.dropdown__list');
+	const dropDownListItems = dropDownList.querySelectorAll('.dropdown__list-item');
+	const dropDownInput = dropDownWrapper.querySelector('.dropdown__input-hidden');
+
+	// Клик по кнопке. Открыть/Закрыть select
+	dropDownBtn.addEventListener('click', function (e) {
+		dropDownList.classList.toggle('dropdown__list--visible');
+        this.classList.add('dropdown__button--active');
+	});
+
+	// Выбор элемента списка. Запомнить выбранное значение. Закрыть дропдаун
+	dropDownListItems.forEach(function (listItem) {
+		listItem.addEventListener('click', function (e) {
+			// e.stopPropagation();
+			dropDownBtn.innerText = this.innerText;
+			dropDownBtn.focus();
+			dropDownInput.value = this.dataset.value;
+			dropDownList.classList.remove('dropdown__list--visible');
+		});
+	});
+
+	// Клик снаружи дропдауна. Закрыть дропдаун
+	document.addEventListener('click', function (e) {
+		if (e.target !== dropDownBtn) {
+			dropDownBtn.classList.remove('dropdown__button--active');
+			dropDownList.classList.remove('dropdown__list--visible');
+		}
+	});
+
+	// Нажатие на Tab или Escape. Закрыть дропдаун
+	document.addEventListener('keydown', function (e) {
+		if (e.key === 'Tab' || e.key === 'Escape') {
+			dropDownBtn.classList.remove('dropdown__button--active');
+			dropDownList.classList.remove('dropdown__list--visible');
+		}
+	});
+});
+
+// value для input range
+
+const slider = document.querySelector('.form__input-range');
+const value = document.querySelector('#value');
+
+value.textContent = slider.value;
+
+slider.oninput = function() {
+	value.textContent = this.value;
+}
